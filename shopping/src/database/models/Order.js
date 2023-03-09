a32ef9243d13dd8a00566ad2b63d0903ae25dc74
Mohit-kavad <mohitkavad@gmail.com>
2023-03-09 16:35:01 +0530

    deleted files from shopping service which are not needed

--- shopping/src/database/models/Order.js
+++ shopping/src/database/models/Order.js
@@ -1,27 +1,33 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const OrderSchema = new Schema({
+const OrderSchema = new Schema(
+  {
     orderId: String,
     customerId: String,
     amount: Number,
     status: String,
     txnId: String,
     items: [
-        {   
-            product: {type: Schema.Types.ObjectId, ref: 'product', required: true} ,
-            unit: { type: Number, require: true} 
-        }
-    ]
-},
-{
+      {
+        product: {
+          type: Schema.Types.ObjectId,
+          ref: "product",
+          required: true,
+        },
+        unit: { type: Number, require: true },
+      },
+    ],
+  },
+  {
     toJSON: {
-        transform(doc, ret){
-            delete ret.__v;
-        }
+      transform(doc, ret) {
+        delete ret.__v;
+      },
     },
-    timestamps: true
-});
+    timestamps: true,
+  }
+);
 
-module.exports =  mongoose.model('order', OrderSchema);
+module.exports = mongoose.model("order", OrderSchema);
